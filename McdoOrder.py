527332d0305357a2e224591a8b9dd77bca37183a
flotr18 <trouilletflorent@outlook.com>
2025-02-02 11:14:50 +0100

    Fix Str spaces

--- McdoOrder.py
+++ McdoOrder.py
@@ -1,5 +1,5 @@
 def UserOrder():
-    print("Welcome to Mcdonald's! What would you like to order?")
+    print("Welcome to Mcdonald's! What would you like to order? ")
     order = input("Would you like to order? (Y/N): ")
     
     if order == "Y":
@@ -7,7 +7,7 @@ def UserOrder():
         if menu == "Y":
             print("Ok Greats! Here is our menu:")
         if menu == "N":
-            print("Ok Greats! Here is our products list instead:")
+            print("Ok Greats! Here is our products list instead: ")
     if order == "N":
         print("Ok! Have a great day!")
 
