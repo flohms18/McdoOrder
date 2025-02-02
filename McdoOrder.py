228dfceba34d8bf4b9a7723075e48dc0a16467b0
flotr18 <trouilletflorent@outlook.com>
2025-02-02 16:59:10 +0100

    SImplified Ordering Process Code

--- McdoOrder.py
+++ McdoOrder.py
@@ -1,122 +1,67 @@
-def UserOrder():
-    SandwichMenu = ["1.Big Mac", "2.Cheeseburger","3.CBO","4.MCChicken","5.Filet-O-Fish"]
-    SideMenu = ["1.Fries","2.Potatoes"]
-    UserOrder = []
-    SideOrder = []
+SandwichMenu = ["1.Big Mac", "2.Cheeseburger","3.CBO","4.MCChicken","5.Filet-O-Fish"]
+SideMenu = ["1.Fries","2.Potatoes"]
+UserOrder = []
 
-
-    print("Welcome to Mcdonald's! What would you like to order? ")
-    order = input("Would you like to order? (Y/N): ")
-    
-    if order == "Y":
-        menu = input("Would you like to order a menu? (Y/N): ")
-        print("Ok! Let me show you our menu:")
-        print(SandwichMenu)
-        UserSandwich = input("What sandwich would you like to order? ")
-        if UserSandwich == "1":
+def SandwichOrder():
+    print(SandwichMenu)
+    UserSandwich = input("What sandwich would you like to order? ")
+    if UserSandwich == "1":
             UserOrder.append(SandwichMenu[0].replace("1.", ""))
             print("You have ordered a Big Mac")
-            side = input("Would you like to add a side? (Y/N): ")
-            if side == "Y":
-                print(SideMenu)
-                UserSide = input("What side would you like to order? ")
-                if UserSide == "1":
-                    SideOrder.append(SideMenu[0].replace("1.", ""))
-                    print("You have ordered Fries")
-                    print("Your order is: ", UserOrder, SideOrder)
-                if UserSide == "2":
-                    SideOrder.append(SideMenu[1].replace("2.", ""))
-                    print("You have ordered Potatoes")
-                    print("Your order is: ", UserOrder, SideOrder)
-            if side == "N":
-                print("Ok! Your order is: ", UserOrder)
-        if UserSandwich == "2":
+    if UserSandwich == "2":
             UserOrder.append(SandwichMenu[1].replace("2.", ""))
             print("You have ordered a Cheeseburger")
-            side = input("Would you like to add a side? (Y/N): ")
-            if side == "Y":
-                print(SideMenu)
-                UserSide = input("What side would you like to order? ")
-                if UserSide == "1":
-                    SideOrder.append(SideMenu[0].replace("1.", ""))
-                    print("You have ordered Fries")
-                    print("Your order is: ", UserOrder, SideOrder)
-                if UserSide == "2":
-                    SideOrder.append(SideMenu[1].replace("2.", ""))
-                    print("You have ordered Potatoes")
-                    print("Your order is: ", UserOrder, SideOrder)
-            if side == "N":
-                print("Ok! Your order is: ", UserOrder)
-        if UserSandwich == "3":
+    if UserSandwich == "3":
             UserOrder.append(SandwichMenu[2].replace("3.", ""))
             print("You have ordered a CBO")
-            side = input("Would you like to add a side? (Y/N): ")
-            if side == "Y":
-                print(SideMenu)
-                UserSide = input("What side would you like to order? ")
-                if UserSide == "1":
-                    SideOrder.append(SideMenu[0].replace("1.", ""))
-                    print("You have ordered Fries")
-                    print("Your order is: ", UserOrder, SideOrder)
-                if UserSide == "2":
-                    SideOrder.append(SideMenu[1].replace("2.", ""))
-                    print("You have ordered Potatoes")
-                    print("Your order is: ", UserOrder, SideOrder)
-            if side == "N":
-                print("Ok! Your order is: ", UserOrder)
-        if UserSandwich == "4":
+    if UserSandwich == "4":
             UserOrder.append(SandwichMenu[3].replace("4.", ""))
             print("You have ordered a MCChicken")
-            side = input("Would you like to add a side? (Y/N): ")
-            if side == "Y":
-                print(SideMenu)
-                UserSide = input("What side would you like to order? ")
-                if UserSide == "1":
-                    SideOrder.append(SideMenu[0].replace("1.", ""))
-                    print("You have ordered Fries")
-                    print("Your order is: ", UserOrder, SideOrder)
-                if UserSide == "2":
-                    SideOrder.append(SideMenu[1].replace("2.", ""))
-                    print("You have ordered Potatoes")
-                    print("Your order is: ", UserOrder, SideOrder)
-            if side == "N":
-                print("Ok! Your order is: ", UserOrder)
-        if UserSandwich == "5":
+    if UserSandwich == "5":
             UserOrder.append(SandwichMenu[4].replace("5.", ""))
             print("You have ordered a Filet-O-Fish")
-            side = input("Would you like to add a side? (Y/N): ")
-            if side == "Y":
-                print(SideMenu)
-                UserSide = input("What side would you like to order? ")
-                if UserSide == "1":
-                    SideOrder.append(SideMenu[0].replace("1.", ""))
-                    print("You have ordered Fries")
-                    print("Your order is: ", UserOrder, SideOrder)
-                if UserSide == "2":
-                    SideOrder.append(SideMenu[1].replace("2.", ""))
-                    print("You have ordered Potatoes")
-                    print("Your order is: ", UserOrder, SideOrder)
-            if side == "N":
-                print("Ok! Your order is: ", UserOrder)
-        if UserSandwich == "2":
-            UserOrder.append(SandwichMenu[1].replace("2.", ""))
-        if UserSandwich == "3":
-            UserOrder.append(SandwichMenu[2].replace("3.", ""))
-        if UserSandwich == "4":
-            UserOrder.append(SandwichMenu[3].replace("4.", ""))
-        if UserSandwich == "5":
-            UserOrder.append(SandwichMenu[4].replace("5.", ""))
-        if menu == "N":
+    side = input("Would you like to add a side? (Y/N): ")
+    if side == "Y":
+        SideOrder()
+    else:
+        print("Ok! Your order is: ", UserOrder)
+
+def SideOrder():
+        print(SideMenu)
+        UserSide = input("What side would you like to order? ")
+        if UserSide == "1":
+            UserOrder.append(SideMenu[0].replace("1.", ""))
+            print("You have ordered Fries")
+            print("Ok! Your order is: ", UserOrder)
+
+        if UserSide == "2":
+            UserOrder.append(SideMenu[1].replace("2.", ""))
+            print("You have ordered Potatoes")
+            print("Ok! Your order is: ", UserOrder)
+
+
+def Order():
+    print("Welcome to Mcdonald's! ")
+    order = input("Would you like to order? (Y/N): ")
+    if order == "Y":
+        SandwichOrder()
+        if order == "N":
             Products = []
             Products.append(SandwichMenu.replace("1.", ""), SandwichMenu.replace("2.", ""), SandwichMenu.replace("3.", ""), SandwichMenu.replace("4.", ""), SandwichMenu.replace("5.", ""))
             Products.append(SideMenu.replace("1.", ""), SideMenu.replace("2.", ""))
             print("Ok Greats! Here is our products list instead: ")
-            print
+            print(Products)
     if order == "N":
         print("Ok! Have a great day!")
 
-UserOrder()
-    
+
+Order()
+
+
+
+
+
+
 
 
 
