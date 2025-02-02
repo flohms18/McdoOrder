e861825c032cd92da6e73294b7ff9ccea305aeb6
flotr18 <trouilletflorent@outlook.com>
2025-02-02 11:23:46 +0100

    Menu Added

--- McdoOrder.py
+++ McdoOrder.py
@@ -1,9 +1,15 @@
 def UserOrder():
+    Sandwichmenu = ["Big Mac", "Cheeseburger","CBO","MCChicken","Filet-O-Fish"]
+    UserOrder = []
     print("Welcome to Mcdonald's! What would you like to order? ")
     order = input("Would you like to order? (Y/N): ")
-    
     if order == "Y":
         menu = input("Would you like to order a menu? (Y/N): ")
+        print("Ok! Let me show you our menu:")
+        print(menu)
+        UserSandwich = input("What sandwich would you like to order? ")
+
+        
         if menu == "Y":
             print("Ok Greats! Here is our menu:")
         if menu == "N":
