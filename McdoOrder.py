2f6cfe9653fa66c5035d2c9787e3377239619581
flotr18 <trouilletflorent@outlook.com>
2025-02-02 11:14:20 +0100

    Create McdoOrder.py

---
+++ McdoOrder.py
@@ -0,0 +1,19 @@
+def UserOrder():
+    print("Welcome to Mcdonald's! What would you like to order?")
+    order = input("Would you like to order? (Y/N): ")
+    
+    if order == "Y":
+        menu = input("Would you like to order a menu? (Y/N): ")
+        if menu == "Y":
+            print("Ok Greats! Here is our menu:")
+        if menu == "N":
+            print("Ok Greats! Here is our products list instead:")
+    if order == "N":
+        print("Ok! Have a great day!")
+
+UserOrder()
+    
+
+
+
+
